f7168930e342a4e365e125aa0cc510d3fff5c562
Ray Milburn <ray.milburn@icloud.com>
2024-08-28 10:43:56 -0400

    Added RectangleTest.java file.

---
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -0,0 +1,23 @@
+package edu.ithaca.dragon.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class RectangleTest {
+    @Test
+    public void calcAreaTest(){
+
+    }
+
+    public void doubleSizeTest(){
+
+    }
+
+    public void longestLineWithinTest(){
+        
+    }
+}
