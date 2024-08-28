2e4e5798a4fe8836d42c9f198af700e5e86934b6
Ray Milburn <ray.milburn@icloud.com>
2024-08-28 10:47:02 -0400

    Added tests to calcAreaTest.

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -10,14 +10,21 @@ import org.junit.jupiter.api.Test;
 public class RectangleTest {
     @Test
     public void calcAreaTest(){
+        Rectangle rect = new Rectangle(3, 4);
+        assertEquals(12, rect.calcArea(), 0.001);
 
+        rect = new Rectangle(5, 5);
+        assertEquals(25, rect.calcArea(), 0.001);
+
+        rect = new Rectangle(.5, .5);
+        assertEquals(0.25, rect.calcArea(), 0.001);
     }
 
     public void doubleSizeTest(){
-
+        
     }
 
     public void longestLineWithinTest(){
-        
+
     }
 }
