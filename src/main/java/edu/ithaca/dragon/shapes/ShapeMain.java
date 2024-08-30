7e36afc3f59bc11ed15f7a9b6be86f2ad96849b2
Ray Milburn <ray.milburn@icloud.com>
2024-08-30 09:52:24 -0400

    Added Triangle and TriangleTest files. Added empty methods to Triangle.

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -6,5 +6,10 @@ public class ShapeMain {
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+        for(int i=0; i<5; i++){
+            Rectangle r = new Rectangle(i, i);
+            System.out.println(r.calcArea());
+            System.out.println(r.longestLineWithin());
+        }       
     }
 }
---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,25 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    private double sideOne;
+    private double sideTwo;
+    private double sideThree;
+
+    public Triangle(double side1, double side2, double side3) {
+        sideOne = side1;
+        sideTwo = side2;
+        sideThree = side3;
+    }
+
+    public double calcArea(){
+        
+    }
+
+    public void doubleSize(){
+
+    }
+
+    public double longestLineWithin(){
+        
+    }
+}
