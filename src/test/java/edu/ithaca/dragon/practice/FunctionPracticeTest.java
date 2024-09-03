bb0d3410a792d122672917ea9a1d075f8197bf48
Ray Milburn <ray.milburn@icloud.com>
2024-09-03 13:34:19 -0400

    Added comments to each test in largestOfThreeTest

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -13,13 +13,26 @@ public class FunctionPracticeTest {
     @Test
     public void largestOfThreeTest(){
         assertEquals(5, FunctionPractice.largestOfThree(4, 5, 2));
+        // Checks for the correct return value given that the largest integer is the second attribute of the object
+
         assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2));
+        // Checks for the correct return value given that the largest integer is the first attribute of the object
+
         assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5));
+        // Checks for the correct return value given that the largest integer is the third attribute of the object
+
         assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5));
+        // Checks for the correct return value given that all attribute values are equal
+
 
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1));
+        // Checks to see if an IllegalArgumentException is thrown when all values are negative
+
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3));
+        // Checks to see if an IllegalArgumentException is thrown when the second attribute is negative
+
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1));
+        // Checks to see if an IllegalArgumentException is thrown when the third attribute is negative    
     }
 
     @Test
