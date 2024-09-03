a9e0efd581f0784b24f82523ec06182594341c21
Ray Milburn <ray.milburn@icloud.com>
2024-09-03 13:58:41 -0400

    Added adjustment to Triangle class to ensure all objects are triangles.

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -6,6 +6,9 @@ public class Triangle {
     private double sideThree;
 
     public Triangle(double side1, double side2, double side3) {
+        if(side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2){
+            throw new IllegalArgumentException("That's not a triangle! No side may be larger than the other two combined.");
+        }
         sideOne = side1;
         sideTwo = side2;
         sideThree = side3;
