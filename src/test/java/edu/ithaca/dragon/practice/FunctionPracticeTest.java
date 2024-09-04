4a21a1da18fb9f137ca322555e63664cd87a9568
Ray Milburn <ray.milburn@icloud.com>
2024-09-04 09:35:03 -0400

    Added tests to calcSalePrice.

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -36,7 +36,26 @@ public class FunctionPracticeTest {
 
     @Test
     public void calcSalePriceTest(){
-        fail("Not implemented yet");
+        assertEquals(80.0, FunctionPractice.calcSalePrice(100.0, 20.0, 0));
+        // Checks if discount is applied to originalPrice properly
+
+        assertEquals(105.0, FunctionPractice.calcSalePrice(100.0, 0, 5.0));
+        // Checks if sales tax is applied to originalPrice properly
+
+        assertEquals(94.5, FunctionPractice.calcSalePrice(100.0, 10, 5.0));
+        // Checks if discount and tax are applied to originalPrice properly in that order
+
+        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.calcSalePrice(0, 0, 0));
+        // Ensures originalPrice cannot be less than or equal to zero
+
+        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.calcSalePrice(-1, 0, 0));
+        // Ensures originalPrice cannot be less than or equal to zero
+
+        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.calcSalePrice(10, -1, 0));
+        // Ensures discountPercent cannot be less than zero
+
+        assertThrows(IllegalArgumentException.class, () -> FunctionPractice.calcSalePrice(10, 0, -1));
+        // Ensures salesTax cannot be less than or equal to zero
     }
 
     @Test
